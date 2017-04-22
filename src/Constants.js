8cf98109b3d413f88cee41fb83af1cfb5b5187d1
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-04-22 14:27:43 +0200

    fix size

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const Height = 600;
-export const Width = Height * 16/9;
+export const Width = 1024;
+export const Height = 576;
 export const WidthSpriteSheetHero = 36;
 export const HeightSpriteSheetHero = 64;
