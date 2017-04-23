1e4ba0ed8432a6fd2e13917139b1e29523038d9b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-04-23 19:10:21 +0200

    tweak the diplayed text"

--- src/Constants.js
+++ src/Constants.js
@@ -12,11 +12,11 @@ export const TextPosition = {
   y: 20,
   x0: 0,
   y0: 0,
-  x1:200,
+  x1: 300,
   y1: 100
 };
 
 
 export const Code = "9356";
 export const StyleRef = { font: "bold 8px Arial", fill: "#000", boundsAlignH: "center", boundsAlignV: "middle" };
-export const Style = { font: "bold 15px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+export const Style = { font: "bold 18px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
--- src/ConstantsKey.js
+++ src/ConstantsKey.js
@@ -4,20 +4,27 @@ export const Tileset = {
   };
 
 export const Level1 = {
-  path: "level2B.json",
+  path: "LVL_1.json",
   key: "Test",
-  text: "Press the left                                                                            "
+  text: "Press the left coulou coulou                                         "
 };
 
 export const Level2 = {
-  path: "level1.json",
+  path: "LVL_2.json",
+  key: "Test",
+  text: "Press the left mouse button to see what is going on                                       "
+};
+
+export const Level3 = {
+  path: "LVL_3.json",
   key: "Test",
   text: "Press the left mouse button to see what is going on                                       "
 };
 
 export const Levels = {
   Level1,
-  Level2
+  Level2,
+  Level3
 };
 
 export const HeroSprite = {
