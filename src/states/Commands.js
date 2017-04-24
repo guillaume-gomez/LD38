68b6efcadc8d1e684e331ac06253ee32fc246d19
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-04-24 12:35:51 +0200

    move text in commands

--- src/states/Commands.js
+++ src/states/Commands.js
@@ -9,19 +9,21 @@ class Commands extends Phaser.State {
     const redoText = "Left mouse button / Spacebar";
     const eraseLayer =  "B key";
 
-    this.game.add.text(350, 80, "Commands", { font: "bold 52px Arial", fill: "#8cc169", stroke: '#4D4D4D',strokeThickness: 1 });
+    this.game.add.text(350, 50, "Commands", { font: "bold 52px Arial", fill: "#8cc169", stroke: '#4D4D4D',strokeThickness: 1 });
 
-    this.game.add.text(150, 225, "Move", { font: "bold 32px Arial", fill: "#fff" });
-    this.game.add.text(500, 225, moveText, { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(150, 200, "Move", { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(500, 200, moveText, { font: "bold 32px Arial", fill: "#fff" });
 
-    this.game.add.text(150, 300, "Aim", { font: "bold 32px Arial", fill: "#fff" });
-    this.game.add.text(500, 300, aimText, { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(150, 275, "Aim", { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(500, 275, aimText, { font: "bold 32px Arial", fill: "#fff" });
 
-    this.game.add.text(150, 375, "Erase Layer", { font: "bold 32px Arial", fill: "#fff" });
-    this.game.add.text(500, 375, eraseLayer, { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(150, 350, "Erase Layer", { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(500, 350, eraseLayer, { font: "bold 32px Arial", fill: "#fff" });
 
-    this.game.add.text(150, 450, "Rollback Layer", { font: "bold 32px Arial", fill: "#fff" });
-    this.game.add.text(500, 450, redoText, { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(150, 425, "Rollback Layer", { font: "bold 32px Arial", fill: "#fff" });
+    this.game.add.text(500, 425, redoText, { font: "bold 32px Arial", fill: "#fff" });
+
+    this.game.add.text(350, 525, "Press enter to start", { font: "bold 34px Arial", fill: "#fff" });
 
     this.enterButton = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     this.game.input.gamepad.start();
