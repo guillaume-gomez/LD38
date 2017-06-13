47e59d47987174b81c8086cb095b57258ada1d32
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-06-13 20:01:27 +0200

    wip emitter

--- src/states/FinalScene.js
+++ src/states/FinalScene.js
@@ -24,22 +24,29 @@ class FinalScene extends Phaser.State {
 
       this.collisionLayer.resizeWorld();
 
-      this.mapManager = new MapManager(this.map, Levels[`Level6`].lastLayer);
+      this.mapManager = new MapManager(this.map, Levels[`Level666`].lastLayer);
       this.mapManager.setUpCollisionLayer(this.collisionLayer);
 
       const originPositionBadGuy = Width + 300;
       const originPositionBaby = Width + 300;
       const originHeroPosition = - 100;
 
-      this.hero = this.game.add.sprite(originHeroPosition, 370, HeroSprite.key);
-      this.baby = this.game.add.sprite(originPositionBaby, 395, 'baby');
-      this.baby2 = this.game.add.sprite(originPositionBaby + 50, 395, 'baby2');
-      this.badGuy = this.game.add.sprite(originPositionBadGuy, 370, 'baby3');
+      this.hero = this.game.add.sprite(originHeroPosition, 405, HeroSprite.key);
+      this.baby = this.game.add.sprite(originPositionBaby, 420, 'baby');
+      this.baby2 = this.game.add.sprite(originPositionBaby + 50, 420, 'baby2');
+      this.badGuy = this.game.add.sprite(originPositionBadGuy, 405, 'baby3');
 
-      this.baby.scale.setTo(BabyRatio, BabyRatio);
-      this.baby2.scale.setTo(BabyRatio, BabyRatio);
-      this.badGuy.scale.setTo(HeroRatio, HeroRatio);
+      this.baby.scale.setTo(-BabyRatio, BabyRatio);
+      this.baby2.scale.setTo(-BabyRatio, BabyRatio);
+      this.badGuy.scale.setTo(-HeroRatio, HeroRatio);
       this.hero.scale.setTo(HeroRatio, HeroRatio);
+
+      console.log(this.hero.anchor)
+      this.baby.anchor.setTo(0.5, 0.5);
+      this.baby2.anchor.setTo(0.5, 0.5);
+      this.badGuy.anchor.setTo(0.5, 0.5);
+      this.hero.anchor.setTo(0.5, 0.5);
+
       const timer = 3000;
 
       let tweenA = this.game.add.tween(this.hero).to( { x: 200 }, timer, "Quart.easeOut");
@@ -47,13 +54,11 @@ class FinalScene extends Phaser.State {
       let tweenC = this.game.add.tween(this.baby).to( { x: 900 - 30 }, timer, "Quart.easeOut");
       let tweenD = this.game.add.tween(this.baby2).to( { x: 900 + 30 }, timer, "Quart.easeOut");
 
-      this.tweenE = this.game.add.tween(this.baby).to( { x: 200 - 30 }, timer, "Quart.easeOut");
-      this.tweenF = this.game.add.tween(this.baby2).to( { x: 200 - 50 }, timer, "Quart.easeOut");
+      this.tweenE = this.game.add.tween(this.baby).to( { x: originHeroPosition }, timer, "Quart.easeOut");
+      this.tweenF = this.game.add.tween(this.baby2).to( { x: originHeroPosition }, timer, "Quart.easeOut");
 
-      this.tweenG = this.game.add.tween(this.hero).to( { x: originHeroPosition }, timer, "Quart.easeOut");
-      this.tweenH = this.game.add.tween(this.baby).to( { x: originHeroPosition - 30 }, timer, "Quart.easeOut");
-      this.tweenI = this.game.add.tween(this.baby2).to( { x: originHeroPosition - 50 }, timer, "Quart.easeOut");
-      this.tweenJ = this.game.add.tween(this.badGuy).to( { x: originPositionBadGuy }, timer, "Quart.easeOut");
+      this.tweenG = this.game.add.tween(this.hero).to( { x: originHeroPosition }, timer * 1.5, "Quart.easeOut");
+      this.tweenJ = this.game.add.tween(this.badGuy).to( { x: originPositionBadGuy }, timer * 1.5, "Quart.easeOut");
 
       tweenD.onComplete.add(this.catched, this);
 
@@ -61,34 +66,36 @@ class FinalScene extends Phaser.State {
       tweenB.start();
       tweenC.start();
       tweenD.start();
+
+      this.emitter = this.game.add.emitter(200, 200, 200);
+      //gems
+      this.emitter.makeParticles(["baby", "baby2"], 10);
+      this.emitter.minParticleSpeed.set(0, 300);
+      this.emitter.maxParticleSpeed.set(0, 400);
+      
   }
 
   catched() {
     this.tweenE.start();
     this.tweenF.start();
+    this.emitter.start(false, 50, 20);
     this.tweenF.onComplete.add(() => {
       this.tweenG.start();
-      this.tweenH.start();
-      this.tweenI.start();
       this.tweenJ.start();
+      //flips
+      this.badGuy.scale.setTo(HeroRatio, HeroRatio);
+      this.hero.scale.setTo(-HeroRatio, HeroRatio);
     }, this);
 
-    // this.tweenH.onComplete.add(() => {
-    //   this.camera.fade(0x000000, 300, false);
-    //   this.game.camera.onFadeComplete.addOnce(this.resetFade([this.tweenF, this.tweenI]), this);
-    //   this.mapManager.removeLayer();
-
-    //   this.badGuy.x = 150;
-    //   this.baby.x = 130;
-    //   this.baby2.kill();
-
-    // }, this);
+     this.tweenJ.onComplete.add(() => {
+        this.game.goToMenu();
+     });
   }
 
   preload() {
     this.game.load.spritesheet(HeroSprite.key, `res/${HeroSprite.path}`, WidthSpriteSheetHero, HeightSpriteSheetHero);
     this.game.load.image(Tileset.key, `res/${Tileset.path}`);
-    this.game.load.tilemap(Levels[`Level6`].key, `res/${Levels[`Level6`].path}` , null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.tilemap(Levels[`Level666`].key, `res/${Levels[`Level666`].path}` , null, Phaser.Tilemap.TILED_JSON);
     this.game.load.image("baby", "res/baby.png");
     this.game.load.image("baby2", "res/baby2.png");
     this.game.load.image("baby3", "res/baby3.png");
